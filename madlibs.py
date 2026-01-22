5fe6e5383702c32cca3beb68fd32b9474bfb5d0f
Meri Kasprak <mkasprak@mchenry.edu>
2026-01-22 12:15:50 -0600

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,33 @@
+"""
+
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS - Madlibs
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Assignment Name, Date, File Name).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+
+# 🐑 Mary had a little lamb 
+
+
+print("\n\n\nMary had a little lamb")
+print("         by: Sarah Josepha Hale")
+
+
+# 🎈🎈🎈 Variables
+
+person_name = input("Person's name:  ")
+animal = input("Type of animal:  ")
+color = input("Color:  ")
+
+
+# 📤 output
+
+print(f"{person_name} had a little {animal}")
+print(f"Whose fleece was {color} as snow.")
+
