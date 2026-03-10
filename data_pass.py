9afad33e0ea7df0cc4f2b542338dc8251a21f4a2
Meri Kasprak <mkasprak@mchenry.edu>
2026-03-10 11:49:18 -0500

    Add initial implementation of greeting and order system functions

---
+++ data_pass.py
@@ -0,0 +1,18 @@
+    """_summary_
+    functions with data
+    """
+    
+    
+def greeting(name)
+    print(f"Welcome to introduction to programming, {name}")
+    #return status
+
+def main():
+    identification = input("Hi friend! What is your name: ")
+    greeting(identification)
+    identification = "Quinn"
+    print(name)# would not work, out of scope
+    
+    
+main()    
+    
